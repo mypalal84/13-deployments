862e0580bcb2e4555f03efd558abc9dad2eaa970
Alex Cahn <alexcahn@gmail.com>
2017-02-22 16:18:56 -0800

    token in env

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,11 +7,7 @@
 
   repos.requestRepos = function(callback) {
     // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
+    $.get('/github.com/user/repos')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
